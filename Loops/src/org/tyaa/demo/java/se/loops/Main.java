2f92c02f8e58ad1f325627a375728c07b03db624
YuriiTrofimenko <tyaa@ukr.net>
2021-08-11 15:37:15 +0300

    loops step 1

---
+++ Loops/src/org/tyaa/demo/java/se/loops/Main.java
@@ -0,0 +1,24 @@
+package org.tyaa.demo.java.se.loops;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        } */
+
+        /* int i = 1;
+        // int i = 11;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10); */
+
+        // int i = 1;
+        int i = 11;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
